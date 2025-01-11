4ade7351f894b65787f725d2c0b6fee9a7d26d24
KosseIA <ir@ikosse.ru>
2025-01-11 22:35:13 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -2,11 +2,11 @@ username = 'Ирина'
 title = 'Список покупок:'
 content = 'соль', 'греча', 'кура'
 status = 'Выполнено'
-created_date = '08-01-2025'
-issue_date = '10-01-2025'
-print('Имя пользователя:', username)
-print('Заголовок заметки:', title)
-print('Описание заметки:', content)
-print('Статус заметки:', status)
-print('Дата создания:', created_date)
-print('Дата истечения:',issue_date)
+created_date = '08.01.2025'
+issue_date = '10.01.2025'
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания: ', created_date)
+print('Дата истечения: ',issue_date)
