9053ffc6b6411ee514559cf4de58b14f7cb04cde
KosseIA <ir@ikosse.ru>
2025-01-11 11:18:20 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+
+username = 'Ирина'
+title = 'Список покупок:'
+content = 'соль', 'греча', 'кура'
+status = 'Выполнено'
+created_date = '08-01-2025'
+issue_date = '10-01-2025'
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print(created_date)
+print(issue_date)
