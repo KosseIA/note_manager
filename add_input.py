b20b06c22f25049b53764b8d4987174fc10f1433
KosseIA <ir@ikosse.ru>
2025-01-17 20:21:40 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,12 @@
+username = input('Введите ваше имя: ')
+title = input('Введите заголовок заметки: ')
+content = input('Введите содержание заметки: ')
+status = input('Выполнено!')
+created_date = '08-01-2025'
+issue_date = '10-01-2025'
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания: ', created_date [0:5])
+print('Дата истечения: ',issue_date [0:5])
