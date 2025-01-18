fd1f55846ef8156f66cf897e3d1ce1bb41d3969d
KosseIA <ir@ikosse.ru>
2025-01-18 13:35:40 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+now = datetime.now()
+only_date = now.date()
+
+username = input('Введите ваше имя: ')
+title = input('Введите заголовок заметки: ')
+title2 = input('Введите заголовок второй заметки: ')
+title3 = input('Введие заголовок третьей заметки: ')
+title4 = input('Введите заголовок четвертой заметки: ')
+content = input('Введите содержание заметки: ')
+status = 'Выполнено!'
+created_date = now
+issue_date = input('Введите дату дедлайна: ')
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания: ', only_date)
+print('Дата дедлайна: ',issue_date)
